9d94249f00b7d50df15d817fc1d18ae74c9ea9c5
ozanenginoglu <ozanenginoglu@Macbook-Pro-Ozan-Enginoglu.local>
2015-05-30 10:27:07 +0300

    ilk çalışır versiyon
    karikatürURL, karikatürİndir fonksiyonları eklendi ve düzgün çalışır
    hale getirildi.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
-import urllib.request
+import os
 import datetime
-from time import sleep, strftime
+import argparse
+import urllib.request
 from bs4 import BeautifulSoup
+from time import sleep, strftime
+
+if not os.path.exists('arşiv'):
+    os.makedirs('arşiv')
 
 def sayfayıYükle(tarih):
     url = 'http://www.komikaze.net/komikaze/'
@@ -19,10 +24,32 @@ def sayfayıYükle(tarih):
     return soup
 
 def öncekiGün(tarih):
-    bugün = datetime.date(*map(int, tarih.split('-')))
-    öncekiGün = bugün - datetime.timedelta(days=1)
+    tarih = datetime.date(*map(int, tarih.split('-')))
+    öncekiGün = tarih - datetime.timedelta(days=1)
     öncekiGün.strftime('%Y-%m-%d')
-    return öncekiGün
+    return str(öncekiGün)
 
-öncekiGün()
+def karikatürURL(soup):
+    link = soup.find('img', {'class': 'caricature-img'})
+    url = link['src']
+    alt = link['alt']
+    return alt, url
+
+def karikatürİndir(URL, öncekiURL, tarih):
+    if URL[1] != öncekiURL:
+        try:
+            karikatür = urllib.request.urlretrieve(URL[1], 'arşiv/' + URL[0] + '.jpg')
+            print('[+] ' + URL[0] + ' - ' + URL[1] + ' [' + tarih + ']')
+        except:
+            print('[-] Karikatür indirilirken problem oluştu. URL: ' + URL[1])
+
 tarih = strftime('%Y-%m-%d')
+kullanıcıGirişi = int(input('Kaç günlük karikatür arşivi indirilecek >> '))
+geçiciDeğişken = ''
+
+for i in range(kullanıcıGirişi):
+    soup = sayfayıYükle(tarih)
+    çıktı = karikatürURL(soup)
+    karikatürİndir(çıktı, geçiciDeğişken, tarih)
+    geçiciDeğişken = çıktı[1]
+    tarih = öncekiGün(tarih)
